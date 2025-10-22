29d63f7f4dbea0ac764b1c2e5f17b8fa896f825f
AdamRepasky <adamson2326@gmail.com>
2025-10-23 01:13:26 +0200

    add loader & min height to task list

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -14,27 +14,42 @@ const TaskList: React.FC<TaskListProps> = ({ currentTask, queuedTasks, completed
       {/* Current Task */}
       <div className="mb-4">
         <h3>Currently Processing</h3>
-        {currentTask ? (<TaskItem task={currentTask} isCurrent />) : <p>No task running.</p>}
+        <div className="min-height-task">
+          {currentTask ? (
+          <TaskItem task={currentTask} isCurrent />
+          ) : queuedTasks.length === 0 ? (
+          <p>No task running.</p>
+          ) : (
+          <>
+              <div className="spinner-grow spinner-grow-sm" role="status"></div>
+              <span> Waiting to start next task…</span>
+          </>
+          )}
+        </div>
       </div>
 
       {/* Queue */}
       <div className="mb-4">
         <h3>Queue (by priority)</h3>
-        {queuedTasks.length === 0 ? (
-          <p>No tasks in queue</p>
-        ) : (
-          queuedTasks.map((task) => <TaskItem key={task.id} task={task} />)
-        )}
+        <div className="min-height-task">
+          {queuedTasks.length === 0 ? (
+            <p>No tasks in queue</p>
+          ) : (
+            queuedTasks.map((task) => <TaskItem key={task.id} task={task} />)
+          )}
+        </div>
       </div>
 
       {/* Completed */}
       <div className="mb-4">
         <h3>Completed Tasks</h3>
-        {completedTasks.length === 0 ? (
-          <p>No completed tasks</p>
-        ) : (
-          completedTasks.map((task) => <TaskItem key={task.id} task={task} />)
-        )}
+        <div className="min-height-task">
+          {completedTasks.length === 0 ? (
+            <p>No completed tasks</p>
+          ) : (
+            completedTasks.map((task) => <TaskItem key={task.id} task={task} />)
+          )}
+        </div>
       </div>
     </div>
   );
